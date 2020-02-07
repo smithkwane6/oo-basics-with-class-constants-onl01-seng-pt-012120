0ff21dd2c933a6f3d9614a908ff897d23cd39fce
Kwane Smith <smithkwane6@gmail.com>
2020-02-07 16:16:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,20 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brands
 
-    attr_accessor :color, :size, :material, :condition
-    attr_reader :title
+BRANDS = []
 
-    def initialize(title)
-        @title = title
-    end
-
-    def brand
-        @title
-    end
-
-    def cobble
-        puts "Your shoe is as good as new!"
-        @condition = "new"
-    end
-
-
-
-
+  def initialize(brand)	  
+    @brand = brand	
+    BRANDS << @brands unless BRANDS.include?(@brands)
+  end	  
 end
+
+
+  def cobble	 
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end	  
+end
+
+
